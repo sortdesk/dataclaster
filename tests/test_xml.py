a44aa96621a75888867819bfbdae0377b7bc65ab
Pierre Monico <pierre@sortdesk.com>
2021-09-07 14:45:17 +0200

    Implement simplest version of from_xml

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -36,11 +36,11 @@ class TestXMLMixin(unittest.TestCase):
     def test_from_xml_attributes(self):
         note_dc = self.Note.from_xml(self.note_tree)
 
-        all_attributes_match = all(
+        all_attributes_match = all([
             self.note_tree.findtext("recipient") == note_dc.recipient,
             self.note_tree.findtext("sender") == note_dc.sender,
             self.note_tree.findtext("heading") == note_dc.heading,
             self.note_tree.findtext("body") == note_dc.body
-        )
+        ])
 
         self.assertTrue(all_attributes_match)
--- xml_mixin.py
+++ xml_mixin.py
@@ -1,4 +1,11 @@
+from dataclasses import fields
+
+
 class XMLMixin:
     @classmethod
-    def from_xml(self, xml_doc):
-        pass
+    def from_xml(cls, xml_tree):
+        parameters = {
+            field.name: xml_tree.findtext(field.name)
+            for field in fields(cls)
+        }
+        return cls(**parameters)
