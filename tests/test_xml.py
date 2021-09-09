5126ac8c16ec2d585f8cef307bf0f5d517264563
Pierre Monico <pierre@sortdesk.com>
2021-09-09 16:14:35 +0200

    Implement fieldwrapper function

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -1,13 +1,13 @@
-import unittest
+from unittest.case import TestCase
 
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
 
 
-from xml_mixin import XMLMixin
+from xml_mixin import XMLMixin, Config, fieldwrapper
 
 
-class TestXMLMixin(unittest.TestCase):
+class TestXMLMixin(TestCase):
 
     def setUp(self) -> None:
         @dataclass
@@ -44,3 +44,45 @@ class TestXMLMixin(unittest.TestCase):
         ])
 
         self.assertTrue(all_tags_match)
+
+    # def test_from_xml_attributes(self):
+    #     @dataclass
+    #     class NoteWithAttributes(self.Note):
+    #         recipient_gender: str = field(metadata=XMLConfig({"xpath": "./recipient", "attrib": "gender"}))
+    #         sender_gender: str = field(metadata=XMLConfig({"xpath": "./sender", "attrib": "gender"}))
+    #         body_lang: str = field(metadata=XMLConfig({"xpath": "./body", "attrib": "lang"}))
+
+    #     note_dc = NoteWithAttributes.from_xml(self.note_tree)
+
+    #     all_attributes_match = all([
+    #         self.note_tree.find("recipient").get("gender") == note_dc.recipient_gender,
+    #         self.note_tree.find("sender").get("gender") == note_dc.sender_gender,
+    #         self.note_tree.find("body").get("lang") == note_dc.body_lang
+    #     ])
+
+    #     self.assertTrue(all_attributes_match)
+
+
+class TestFieldConfig(TestCase):
+
+    def test_fieldwrapper_preserves_metadata(self):
+        config = Config()
+
+        @dataclass
+        class Something:
+            name: str = fieldwrapper(config=config, metadata={"hello": "world"})
+
+        something = Something(name="Pierre")
+
+        self.assertTrue(something.__dataclass_fields__["name"].metadata["hello"] == "world")
+
+    def test_fieldwrapper_passes_config(self):
+        config = Config()
+
+        @dataclass
+        class Something:
+            name: str = fieldwrapper(config=config, metadata={"hello": "world"})
+
+        something = Something(name="Pierre")
+
+        self.assertTrue(isinstance(something.__dataclass_fields__["name"].metadata["_config"], Config))
--- xml_mixin.py
+++ xml_mixin.py
@@ -1,11 +1,46 @@
-from dataclasses import fields
+from dataclasses import fields, field as dcfield
+from typing import Type
+import inspect
 
 
 class XMLMixin:
     @classmethod
+    def process_standard_field(cls, field, xml_tree):
+        return xml_tree.findtext(field.name)
+
+    @classmethod
+    def process_complex_field(cls, field, xml_tree):
+        xpath = field.metadata["xpath"]
+        attrib = field.metadata["attrib"]
+        element = xml_tree.find(xpath)
+        print(element)
+        if attrib:
+            return element.get(attrib)
+        else:
+            return element.text
+
+    @classmethod
+    def process_field(cls, field, xml_tree):
+        print(inspect.getmro(type(field.metadata)))
+        if isinstance(field.metadata, Config):
+            print("hello")
+            return cls.process_complex_field(field, xml_tree)
+        else:
+            # TODO: maybe don't process at all?
+            return cls.process_standard_field(field, xml_tree)
+
+    @classmethod
     def from_xml(cls, xml_tree):
-        parameters = {
-            field.name: xml_tree.findtext(field.name)
-            for field in fields(cls)
-        }
-        return cls(**parameters)
+        dc = cls(**{field.name: cls.process_field(field, xml_tree) for field in fields(cls)})
+        # print(dc)
+        return dc
+
+
+class Config:
+    pass
+
+
+def fieldwrapper(config: Type[Config], *args, **kwargs):
+    # TODO: this will break if `field()` is called with args only
+    kwargs["metadata"]["_config"] = config
+    return dcfield(*args, **kwargs)
