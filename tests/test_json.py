60fd28cdd28501375a1e9793256652137f8b9439
Pierre Monico <pierre@sortdesk.com>
2021-10-21 19:30:06 +0200

    Write failing test for simple properties

--- tests/test_json.py
+++ tests/test_json.py
@@ -26,8 +26,21 @@ class TestJSONMixinWithFlatDocument(unittest.TestCase):
         except Exception as e:
             self.fail(e)
 
-    def test_from_json(self):
+    def test_from_dict(self):
         try:
             self.Note.from_dict(self.note_dict)
         except Exception as e:
             self.fail(e)
+
+    def test_from_dict_properties(self):
+        note_dc = self.Note.from_dict(self.note_dict)
+
+        all_properties_match = all([
+            self.note_dict["recipient"] == note_dc.recipient,
+            self.note_dict["sender"] == note_dc.sender,
+            self.note_dict["heading"] == note_dc.heading,
+            self.note_dict["body"] == note_dc.body,
+            self.note_dict["priority"] == note_dc.priority
+        ])
+
+        self.assertTrue(all_properties_match)
