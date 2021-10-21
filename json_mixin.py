b4318e5e8bc0a19d3d1e524c9428abe067ac4950
Pierre Monico <pierre@sortdesk.com>
2021-10-21 19:34:13 +0200

    Implement flat json support

--- json_mixin.py
+++ json_mixin.py
@@ -1,5 +1,13 @@
+from dataclasses import fields
+
+
 class JSONMixin:
 
     @classmethod
-    def from_dict(cls, dictionary):
-        pass
+    def process_field(cls, field, json_dict):
+        return json_dict[field.name]
+
+    @classmethod
+    def from_dict(cls, json_dict):
+        dc = cls(**{field.name: cls.process_field(field, json_dict) for field in fields(cls)})
+        return dc
