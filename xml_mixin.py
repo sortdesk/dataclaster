8c2f9bf7446300eedfe625e28e3047b4f6898942
Pierre Monico <pierre@sortdesk.com>
2021-12-04 14:31:23 +0100

    Make relevant xmlmixin methods private

--- xml_mixin.py
+++ xml_mixin.py
@@ -12,16 +12,16 @@ class XMLConfig(Config):
 class XMLMixin(BaseMixin):
 
     @classmethod
-    def get_text_values(cls, elements):
+    def _get_text_values(cls, elements):
         text_values = []
 
         for element in elements:
-            text_values.append(cls.get_text_value(element))
+            text_values.append(cls._get_text_value(element))
 
         return text_values
 
     @classmethod
-    def get_text_value(cls, element):
+    def _get_text_value(cls, element):
         # TODO: this try/except is a mess
         try:
             return element.text
@@ -29,7 +29,7 @@ class XMLMixin(BaseMixin):
             return str(element)
 
     @classmethod
-    def process_field(cls, field, xml_tree):
+    def _process_field(cls, field, xml_tree):
 
         cls.raise_for_types_not_supported(field.type)
 
@@ -38,28 +38,28 @@ class XMLMixin(BaseMixin):
                 raise ValueError(
                     f"You must pass a valid instance of XMLConfig to the `config` parameter on {field.name}"
                 )
-            return cls.process_field_with_config(field, xml_tree)
+            return cls._process_field_with_config(field, xml_tree)
         else:
-            return cls.process_field_without_config(field, xml_tree)
+            return cls._process_field_without_config(field, xml_tree)
 
     @classmethod
-    def process_field_with_config(cls, field, xml_tree):
+    def _process_field_with_config(cls, field, xml_tree):
         base_type = cls.get_base_type(field.type)
         config = field.config
 
         elements = xml_tree.xpath(config.xpath)
         if base_type == list:
-            value = cls.get_text_values(elements)
+            value = cls._get_text_values(elements)
         else:
             if len(elements) > 1:
                 # TODO: improve message + maybe DRY this check with JSON mixin
                 raise TooManyMatchesError(f"There are two many matches for {field.name}.")
-            value = cls.get_text_value(elements[0])
+            value = cls._get_text_value(elements[0])
 
         return cls.cast_value_to_type(value, field.type)
 
     @classmethod
-    def process_field_without_config(cls, field, xml_tree):
+    def _process_field_without_config(cls, field, xml_tree):
         value = xml_tree.findtext(field.name)
         return cls.cast_value_to_type(value, field.type)
 
